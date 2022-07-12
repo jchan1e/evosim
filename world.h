d24cdb4d2d222b9c97836b42a3f7455dd6425a5a
jordan <jchan1e@gmail.com>
2022-07-12 15:42:03 -0600

    fixed simulation flickering and adjusted stochastic plant update rate

--- world.h
+++ world.h
@@ -107,120 +107,123 @@ public:
       for (int j=0; j < gridsize; ++j) {
         Cell* C = &grid[ping][i][j];
         Cell* C_1 = &grid[pong][i][j];
-        if (rand()%10 != 0) {
+        if (rand()%8 != 0) {
+          //C_1->plant = C->plant;
           continue;
         }
-        // plant level 1-3 = grass, grows whether there are neighbors or not and seeds nearby cells
-        if (C->plant >= 1  && C->plant <= 3) {
-          if (C->plant < 3) {
-            C_1->plant = C->plant + 1;
-          }
-          else { // C->plant == 3
-            // check neighbors to see if it can be upgraded to a bush
-            int adj = 0;
-            //if (grid[ping][(i-2+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-2+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-2+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-2+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
-            //if (grid[ping][(i-2+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-1+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i-1+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+0+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+0+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+0+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+0+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+1+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+1+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
-            //if (grid[ping][(i+2+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+2+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+2+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
-            if (grid[ping][(i+2+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
-            //if (grid[ping][(i+2+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
-            if (adj == 20) { // if exactly 20 adjacent neighbors, bushes can spawn
+        else {
+          // plant level 1-3 = grass, grows whether there are neighbors or not and seeds nearby cells
+          if (C->plant >= 1  && C->plant <= 3) {
+            if (C->plant < 3) {
               C_1->plant = C->plant + 1;
             }
-            else { // otherwise seed nearby cell
-              int s_i = (i-2 + rand()%5+gridsize)%gridsize;
-              int s_j = (j-2 + rand()%5+gridsize)%gridsize;
-              if (grid[ping][s_i][s_j].plant == 0)
-                grid[pong][s_i][s_j].plant = 1;
-            }
-          }
-        }
-
-        // plant level 4-6 = bush
-        else if (C->plant >= 4 && C->plant <= 6) {
-          bool grow = false;
-          // check neighbors for conway conditions
-          int adj = 0;
-          if (grid[ping][(i-1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i-1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i-1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i+0+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i+0+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i+1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i+1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant > 3) adj++;
-          if (grid[ping][(i+1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
-          //if (adj == 2 || adj == 3) {
-          if (adj < 2) {
-            grow = true;
-          }
-          if (grow) {
-            if (C->plant < 6) {
-              C_1->plant = C->plant + 1;
-            }
-            else { //c->plant == 6
-              // check for nearby trees
-              int t_adj = 0;
-              for (int ii = i-4; ii <= i+4; ++ii) {
-                for (int jj = j-4; jj <= j+4; ++jj) {
-                  if (abs(ii-i) + abs(jj-j) < 6) {
-                    if (grid[ping][(ii+gridsize)%gridsize][(jj+gridsize)%gridsize].plant > 5) t_adj++;
-                  }
-                }
+            else { // C->plant == 3
+              // check neighbors to see if it can be upgraded to a bush
+              int adj = 0;
+              //if (grid[ping][(i-2+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-2+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-2+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-2+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
+              //if (grid[ping][(i-2+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-1+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i-1+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+0+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+0+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+0+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+0+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+1+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+1+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
+              //if (grid[ping][(i+2+gridsize)%gridsize][(j-2+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+2+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+2+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant == 3) adj++;
+              if (grid[ping][(i+2+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant == 3) adj++;
+              //if (grid[ping][(i+2+gridsize)%gridsize][(j+2+gridsize)%gridsize].plant == 3) adj++;
+              if (adj == 20) { // if exactly 20 adjacent neighbors, bushes can spawn
+                C_1->plant = C->plant + 1;
               }
-              if (t_adj > 2) {
-                // seed random cell within distance 3
-                int s_i = (i-3 + rand()%7+gridsize)%gridsize;
-                int s_j = (j-3 + rand()%7+gridsize)%gridsize;
-                if (grid[ping][s_i][s_j].plant == 0)
+              else { // otherwise seed nearby cell
+                int s_i = ((i-2 + rand()%5)+gridsize)%gridsize;
+                int s_j = ((j-2 + rand()%5)+gridsize)%gridsize;
+                if (grid[ping][s_i][s_j].plant < 1)
                   grid[pong][s_i][s_j].plant = 1;
               }
-              else {
-                // upgrade to tree
-                C_1->plant += 1;
-              }
             }
           }
-          else if (adj > 4) { // die if overcrowded
-            C_1->plant = C->plant - 1;
-          }
-          else { // otherwise, no change
-            C_1->plant = C->plant;
-          }
-        }
 
-        // plant level 7-9 = tree, can only grow if < 3 other trees nearby
-        else if (C->plant >= 7 && C->plant <= 9) {
-          int adj = -1; // avoid counting self
-          for (int ii = i-4; ii <= i+4; ++ii) {
-            for (int jj = j-4; jj <= j+4; ++jj) {
-              if (abs(ii-i) + abs(jj-j) < 6) {
-                if (grid[ping][(ii+gridsize)%gridsize][(jj+gridsize)%gridsize].plant > 6) adj++;
+          // plant level 4-6 = bush
+          else if (C->plant >= 4 && C->plant <= 6) {
+            bool grow = false;
+            // check neighbors for conway conditions
+            int adj = 0;
+            if (grid[ping][(i-1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i-1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i-1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i+0+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i+0+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i+1+gridsize)%gridsize][(j-1+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i+1+gridsize)%gridsize][(j+0+gridsize)%gridsize].plant > 3) adj++;
+            if (grid[ping][(i+1+gridsize)%gridsize][(j+1+gridsize)%gridsize].plant > 3) adj++;
+            //if (adj == 2 || adj == 3) {
+            if (adj < 2) {
+              grow = true;
+            }
+            if (grow) {
+              if (C->plant < 6) {
+                C_1->plant = C->plant + 1;
+              }
+              else { //c->plant == 6
+                // check for nearby trees
+                int t_adj = 0;
+                for (int ii = i-4; ii <= i+4; ++ii) {
+                  for (int jj = j-4; jj <= j+4; ++jj) {
+                    if (abs(ii-i) + abs(jj-j) < 6) {
+                      if (grid[ping][(ii+gridsize)%gridsize][(jj+gridsize)%gridsize].plant > 5) t_adj++;
+                    }
+                  }
+                }
+                if (t_adj > 2) {
+                  // seed random cell within distance 3
+                  int s_i = (i-3 + rand()%7+gridsize)%gridsize;
+                  int s_j = (j-3 + rand()%7+gridsize)%gridsize;
+                  if (grid[ping][s_i][s_j].plant == 0)
+                    grid[pong][s_i][s_j].plant = 1;
+                }
+                else {
+                  // upgrade to tree
+                  C_1->plant += 1;
+                }
               }
             }
+            else if (adj > 4) { // die if overcrowded
+              C_1->plant = C->plant - 1;
+            }
+            else { // otherwise, no change
+              C_1->plant = C->plant;
+            }
           }
-          if (adj < 2) { // can grow if 1 or fewer other trees nearby
-            C_1->plant = C->plant + 1;
-          }
-          else if (adj > 4) { // actively dies if 4 or more nearby neighbors
-            C_1->plant = C->plant - 1;
+
+          // plant level 7-9 = tree, can only grow if < 3 other trees nearby
+          else if (C->plant >= 7 && C->plant <= 9) {
+            int adj = -1; // avoid counting self
+            for (int ii = i-4; ii <= i+4; ++ii) {
+              for (int jj = j-4; jj <= j+4; ++jj) {
+                if (abs(ii-i) + abs(jj-j) < 6) {
+                  if (grid[ping][(ii+gridsize)%gridsize][(jj+gridsize)%gridsize].plant > 6) adj++;
+                }
+              }
+            }
+            if (adj < 2) { // can grow if 1 or fewer other trees nearby
+              C_1->plant = C->plant + 1;
+            }
+            else if (adj > 4) { // actively dies if 4 or more nearby neighbors
+              C_1->plant = C->plant - 1;
+            }
           }
         }
       }
